782b01634004086e379d9d2d0685c5dce25e64b6
Andrew Buldakovsky <aabweber@gmail.com>
2014-01-29 15:02:05 +0400

    erlang output in returndata added

--- ReturnData.php
+++ ReturnData.php
@@ -10,15 +10,36 @@ namespace misc;
 
 
 class ReturnData {
-	const STATUS_ERROR      = 'ERROR';
-	const STATUS_OK         = 'OK';
+	const STATUS_ERROR          = 'ERROR';
+	const STATUS_OK             = 'OK';
+
+	const RETURN_FORMAT_JSON    = 'json';
+	const RETURN_FORMAT_ERLANG  = 'erlang';
 
 	private $status;
 	private $code;
 	private $data;
 
+	private function erlang_encode_object($object){
+		$string = '';
+		foreach($object as $key => $value){
+			$string .= '{'.$key.', '.(is_array($value)?$this->erlang_encode_object($value):$value).'}';
+		}
+		$string .= '';
+		return $string;
+	}
+
+	private function erlang_encode($data){
+		return '{'.strtolower($data['status']).', '.$data['code'].', '.$this->erlang_encode_object($data['data']).'}';
+	}
+
 	function __toString(){
-		return json_encode(['status' => $this->status, 'code' => $this->code, 'data' => $this->data]);
+		switch(RETURN_FORMAT){
+			case self::RETURN_FORMAT_JSON:
+				return json_encode(['status' => $this->status, 'code' => $this->code, 'data' => $this->data]);
+			case self::RETURN_FORMAT_ERLANG:
+				return $this->erlang_encode(['status' => $this->status, 'code' => $this->code, 'data' => $this->data]);
+		}
 	}
 
 	static function get($status, $code, $data){
