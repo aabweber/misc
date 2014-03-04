6d05409feb7dd7ac796d9c486daade14f76234d3
Andrew Buldakovsky <aabweber@gmail.com>
2014-03-04 10:10:26 +0400

    utils - checp password & check email added

--- Utils.php
+++ Utils.php
@@ -10,6 +10,11 @@ namespace misc;
 
 
 class Utils {
+
+	static function getArray($arr){
+		return $arr;
+	}
+
 	static function isValueSet($array, $var){
 		return isset($array[$var]) && isset($array[$var][0]) && isset($array[$var][0]['value']);
 	}
@@ -18,5 +23,21 @@ class Utils {
 		if(self::isValueSet($array, $var)){
 			return $array[$var][0]['value'];
 		}
+		return null;
+	}
+
+	static function checkEmail($email){
+		if(preg_match('/^[\.\-_A-Za-z0-9]+?@[\.\-A-Za-z0-9]+?\.[A-Za-z0-9]{2,6}$/', $email)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public static function checkPassword($password) {
+		if(strlen($password)<5){
+			return false;
+		}
+		return true;
 	}
 } 
