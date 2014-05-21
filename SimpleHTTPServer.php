24911e1dd65883eef4c410075e440d83e773d2fc
Andrey Buldakovskiy <aabweber@gmail.com>
2014-05-21 17:10:13 +0400

    SimpleHTTPServer is abstract now

--- SimpleHTTPServer.php
+++ SimpleHTTPServer.php
@@ -9,23 +9,20 @@
 namespace misc;
 
 
-class SimpleHTTPServer extends SocketDaemon{
-	private $my_external_ip;
+abstract class SimpleHTTPServer extends SocketDaemon{
+	private $my_ip;
 	function initInstance(){
 		parent::initInstance();
-		$config = $this->getConfig()['SERVER'];
-		$this->my_external_ip = Network::getInterfaces($config['external_network'])[0];
-		$this->addServerSocket($this->my_external_ip, $config['external_listen_port']);
+		$config = $this->getConfig()['HTTP_SERVER'];
+		$this->my_ip = Network::getInterfaces($config['network'])[0];
+		$this->addServerSocket($this->my_ip, $config['listen_port']);
 	}
 
 	function loop(){
 		parent::loop();
 	}
 
-	public function processClient(SimpleHTTPClient $client) {
-		$client->setReplyType(SimpleHTTPReply::TYPE_JSON);
-		echo json_encode(['abc'=>@$_REQUEST['abc']]);
-	}
+	abstract function processClient(SimpleHTTPClient $client);
 
 	protected function initDaemon(){
 
