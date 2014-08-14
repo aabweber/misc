c68dd9222950c62c6983ab3c47d399a35a19eb50
Andrew Buldakovsky <aabweber@gmail.com>
2014-08-14 18:01:09 +0400

    some comments were cuted

--- CURL/StreamableURL.php
+++ CURL/StreamableURL.php
@@ -8,7 +8,6 @@
 namespace misc\CURL;
 
 use misc\Timer;
-use Utils;
 
 class StreamableURL extends StreamableContent{
 	private $read_curl;
@@ -22,12 +21,10 @@ class StreamableURL extends StreamableContent{
 		$curl->modify(['timeout'=>0]);
 		$curlInitiatedInMultiCURL = false;
 		$this->read_curl->setDataWriteFunction(function($data) use ($curl, &$curlInitiatedInMultiCURL){
-//			echo '.';
 			$this->buffer .= $data;
 			if(isset($this->buffer[$this->dataPointer+1])){
 				if(!$curlInitiatedInMultiCURL){
 					$curlInitiatedInMultiCURL = true;
-					echo "initiating PUT curl in multiCURL\n";
 					$curl->setNeedInitInMultiCURL(true);
 					Timer::after(0, function() use ($curl){
 						$curl->getMultiCURL()->initCURL($curl);
@@ -40,9 +37,7 @@ class StreamableURL extends StreamableContent{
 			}
 		});
 		$curl->on(CURL::EVENT_PREPARED, function(CURL $curl){
-			echo "PUT curl prepared\n";
 			$curl->clearListeners(CURL::EVENT_PREPARED); // adding only once
-			echo "get curl added to multiCURL\n";
 			$curl->getMultiCURL()->add($this->read_curl, [$this, 'onFinish']);
 		});
 		$curl->setNeedInitInMultiCURL(false);
@@ -64,32 +59,22 @@ class StreamableURL extends StreamableContent{
 		$M = 1024*1024;
 		if($this->dataPointer > $M){
 			$this->buffer = substr($this->buffer, $M);
-			$this->dataPointer-= $M;
+			$this->dataPointer -= $M;
 		}
 		return $str;
 	}
 
-	private $i = 0;
 	function read($length) {
-//		echo "$length ".($this->i++)."\n";
-		if(!$this->buffer){
-			echo "Buffer is empty\n";
-			return '';
-		}
 		if(!isset($this->buffer[$length]) && !$this->finished){
-//			$bufLen = strlen($this->buffer);
-//			$str = Utils::shiftString($this->buffer, $bufLen-1);
 			$str = $this->getData(strlen($this->buffer) - 1);
 			$this->pauseSending();
 		}else{
-//			$str = Utils::shiftString($this->buffer, $length);
 			$str = $this->getData($length);
 		}
 		return $str;
 	}
 
 	function onFinish(){
-		echo "READING FINISHED\n";
 		$this->finished = true;
 		if($this->paused){
 			$this->resumeSending();
